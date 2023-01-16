6abcf61c96ed14bfa1196e6ad787faf7f4dccdc5
Sanju Kamat <sanjukamat123@gmail.com>
2023-01-16 12:05:53 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,193 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.0992931095338818,
+        "pitch": -0.05054268585502264,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.29712081539852875,
+          "pitch": 0.05776816986907107,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": 0.962695922193312,
+          "pitch": 0.08148255260496562,
+          "rotation": 0,
+          "target": "3-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.310356023514423,
+        "pitch": 0.2028105582073998,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1118926895034633,
+          "pitch": 0.04262011370636998,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": -0.4529354847464351,
+          "pitch": 0.10067953632637483,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 1.039674251655585,
+          "pitch": 0.1498000344559216,
+          "rotation": 0,
+          "target": "3-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.4882922519761692,
+        "pitch": -0.10346425600781295,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8148776060855063,
+          "pitch": 0.045065377131889406,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": -2.5760571415536564,
+          "pitch": 0.20618380562319594,
+          "rotation": 0,
+          "target": "3-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.09171296200509893,
+        "pitch": 0.14523226496156738,
+        "fov": 1.1991635784062282
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.039869388305993,
+          "pitch": 0.06724308895531372,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 2.199626563497479,
+          "pitch": 0.015603731511792063,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Lanai Deck",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
